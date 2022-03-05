984f524376579b886b8c6f178609e14468ce4200
Wolfleader <james.morgan@iinet.net.au>
2022-03-05 19:02:27 +0800

    added in an example and fixed cmake

--- src/main.c
+++ src/main.c
@@ -8,5 +8,8 @@ int main()
 	Vector3 vec3 = { 1.0f, 2.0f, 3.0f };
 	printf("Hello World!\n");
 	printf("%f %f %f\n", vec3.x, vec3.y, vec3.z);
+
+	vec3 = Vec3ScalarMultiply(vec3, 12.0f);
+	printf("%f %f %f\n", vec3.x, vec3.y, vec3.z);
 	return 0;
 }
