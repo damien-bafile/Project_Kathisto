aac1233abae99ff321d59f52403f325b47a17d12
Wolfleader <james.morgan@iinet.net.au>
2022-03-05 18:22:09 +0800

    added in my Vector methods

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 ﻿#include <stdlib.h>
 #include <stdio.h>
 
+#include "math/Vector.h"
 
 int main()
 {
-	printf("Hello World!");
+	Vector3 vec3 = { 1.0f, 2.0f, 3.0f };
+	printf("Hello World!\n");
+	printf("%f %f %f\n", vec3.x, vec3.y, vec3.z);
 	return 0;
 }
