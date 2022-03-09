c344930492e61f39356f995359912d8c18e3f8ba
Wolfleader <james.morgan@iinet.net.au>
2022-03-09 17:15:19 +0800

    Update Game.h

--- src/game/Game.h
+++ src/game/Game.h
@@ -8,8 +8,6 @@
 
 #include "math/Vector.h"
 
-
-
 typedef struct Transform
 {
 	Vector3 position;
@@ -49,6 +47,37 @@ typedef struct GameObjectManager
 	GameObject *gameObjects;
 	size_t count;
 } GameObjectManager;
+
+typedef struct IdKeyValue
+{
+	uint32_t id;
+	size_t index;
+} IdKeyValue;
+
+typedef struct IdMap
+{
+	IdKeyValue *values;
+} IdMap;
+
+/*
+	*When a GameObject is added:
+		add the game object to the end of the game object list at index of count
+		add value to the IDMap of gamobjects id : pushed index (count)
+		increase count by 1
+
+	*When a GameObject is removed:
+		first find the index in IdMap where id == id
+		remove the key value from the IdMap
+		remove the gameobject from the game object list by setting it to null
+
+
+
+
+*/
+
+
+
+
 
 void gameObjectManagerAdd(GameObject GameObject);
 void gameObjectManagerRemove(uint32_t id);
